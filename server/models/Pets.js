86af7c308a7094210663e7c769a8834aad7f455a
Juan Pablo Pajaro <juanpablo192712@hotmail.com>
2021-06-15 12:13:56 -0500

    Todo funcional, menos el adoptar mascotas

---
+++ server/models/Pets.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const petSchema = new mongoose.Schema(
+  {
+    nameOwner: {
+      type: String,
+      require: true,
+      maxlength: 30,
+    },
+    name: {
+      type: String,
+      require: true,
+      maxlength: 20,
+    },
+    edad: {
+      type: Number,
+      require: true,
+    },
+    description: {
+      type: String,
+      default: "no hay descripcion",
+    },
+    especie: {
+      type: ObjectId,
+      ref: "Category",
+      require: true,
+    },
+    genero: {
+      type: String,
+      require: true,
+    },
+    raza: {
+      type: String,
+      maxlength: 20,
+      default: "Desconocido",
+    },
+    estado: {
+      type: String,
+      default: "Active",
+    },
+    descriptionState: {
+      type: String,
+      default: "No hay cambios de estado",
+    },
+    foto: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Pets", petSchema);
